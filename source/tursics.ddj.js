340ca02c979df727e9aac96cdc3796bd34e5f083
Thomas Tursics <thomas@tursics.de>
2020-07-14 01:47:22 +0200

    implement the jQuery getJSON function

--- source/tursics.ddj.js
+++ source/tursics.ddj.js
@@ -2,7 +2,7 @@
 /* version 0.4 */
 
 /*jslint browser: true*/
-/*global window,document */
+/*global window,document,XMLHttpRequest */
 
 // -----------------------------------------------------------------------------
 
@@ -36,6 +36,61 @@ var ddj = ddj || {};
 
 	function mapAction() {
 		// nothing to do
+	}
+
+	// -------------------------------------------------------------------------
+
+	function getJSON(uri, successCallback) {
+		var promiseObject = {
+			onDone: null,
+			onFail: null,
+			onAlways: null,
+
+			done: function(callback) {
+				this.onDone = callback;
+				return this;
+			},
+			fail: function(callback) {
+				this.onFail = callback;
+				return this;
+			},
+			always: function(callback) {
+				this.onAlways = callback;
+				return this;
+			},
+		};
+
+		var request = new XMLHttpRequest();
+		request.open('GET', uri, true);
+
+		request.onreadystatechange = function() {
+			if (this.readyState === 4) {
+				if (this.status >= 200 && this.status < 400) {
+					var data = JSON.parse(this.responseText);
+					if (successCallback) {
+						successCallback(data);
+					}
+					if (promiseObject.onDone) {
+						promiseObject.onDone(data);
+					}
+					if (promiseObject.onAlways) {
+						promiseObject.onAlways();
+					}
+				} else {
+					if (promiseObject.onFail) {
+						promiseObject.onFail(/*jqxhr*/null, /*textStatus*/'', /*error*/null);
+					}
+					if (promiseObject.onAlways) {
+						promiseObject.onAlways();
+					}
+				}
+			}
+		};
+
+		request.send();
+		request = null;
+
+		return promiseObject;
 	}
 
 	// -------------------------------------------------------------------------
@@ -178,6 +233,33 @@ var ddj = ddj || {};
 				zoom: ddj.getMetaContent('ddj:mapZoom'),
 				attribution: attribution.length > 0 ? attribution[0].innerHTML : '',
 				onFocusOnce: mapAction
+			});
+		}
+
+		var dataUri = ddj.getMetaContent('ddj:data');
+		if (dataUri) {
+			dataUri = dataUri + '?nocache=' + (new Date().getTime());
+
+			// Assign handlers immediately after making the request,
+			// and remember the jqxhr object for this request
+			var jqxhr = getJSON( dataUri, function() {
+			  console.log( "success" );
+			})
+			  .done(function() {
+				console.log( "second success" );
+			  })
+			  .fail(function() {
+				console.log( "error" );
+			  })
+			  .always(function() {
+				console.log( "complete" );
+			  });
+
+			// Perform other work here ...
+
+			// Set another completion function for the request above
+			jqxhr.always(function() {
+			  console.log( "second complete" );
 			});
 		}
 	}
