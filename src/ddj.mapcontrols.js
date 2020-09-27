aaa69b01f29fd74ae21b42293074d542bdf83a86
Thomas Tursics <thomas@tursics.de>
2020-09-27 23:36:36 +0200

    remove need of jQuery and Leaflet if you load the lib without call any functions

--- src/ddj.mapcontrols.js
+++ src/ddj.mapcontrols.js
@@ -20,35 +20,29 @@ const settings = {
 
 // -----------------------------------------------------------------------------
 
-var ControlInfo = L.Control.extend({
-	options: {
-		position: 'bottomright',
-	},
+function onAddCallback() {
+	var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom'),
+		icon;
 
-	onAdd: function () {
-		var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control leaflet-control-custom'),
-			icon;
-
-		if ($('#popupShare').length === 1) {
-			icon = $('#popupShare').data('icon') || 'fa fa-share-alt';
-			container.innerHTML = '<a style="font-size:1.2em" href="#popupShare" title="Teilen" data-rel="popup" data-position-to="window" data-transition="pop"><i class="' + icon + '" aria-hidden="true"></i></a>';
-		}
-		if ($('#popupInfo').length === 1) {
-			icon = $('#popupInfo').data('icon') || 'fa fa-info';
-			container.innerHTML += '<a style="font-size:1.2em" href="#popupInfo" title="Info" data-rel="popup" data-position-to="window" data-transition="pop"><i class="' + icon + '" aria-hidden="true"></i></a>';
-		}
-		if ($('#popupAuthor').length === 1) {
-			icon = $('#popupAuthor').data('icon') || 'fa fa-envelope';
-			container.innerHTML += '<a style="font-size:1.2em" href="#popupAuthor" title="Autor" data-rel="popup" data-position-to="window" data-transition="pop"><i class="' + icon + '" aria-hidden="true"></i></a>';
-		}
-		if ($('[data-tutorial="dialog"]').length === 1) {
-			icon = $('[data-tutorial="dialog"]').data('icon') || 'fa fa-graduation-cap';
-			container.innerHTML += '<a style="font-size:1.2em" href="#popupTutorial" title="Anleitung" data-rel="popup" data-position-to="window" data-transition="pop"><i class="' + icon + '" aria-hidden="true"></i></a>';
-		}
-
-		return container;
+	if ($('#popupShare').length === 1) {
+		icon = $('#popupShare').data('icon') || 'fa fa-share-alt';
+		container.innerHTML = '<a style="font-size:1.2em" href="#popupShare" title="Teilen" data-rel="popup" data-position-to="window" data-transition="pop"><i class="' + icon + '" aria-hidden="true"></i></a>';
 	}
-});
+	if ($('#popupInfo').length === 1) {
+		icon = $('#popupInfo').data('icon') || 'fa fa-info';
+		container.innerHTML += '<a style="font-size:1.2em" href="#popupInfo" title="Info" data-rel="popup" data-position-to="window" data-transition="pop"><i class="' + icon + '" aria-hidden="true"></i></a>';
+	}
+	if ($('#popupAuthor').length === 1) {
+		icon = $('#popupAuthor').data('icon') || 'fa fa-envelope';
+		container.innerHTML += '<a style="font-size:1.2em" href="#popupAuthor" title="Autor" data-rel="popup" data-position-to="window" data-transition="pop"><i class="' + icon + '" aria-hidden="true"></i></a>';
+	}
+	if ($('[data-tutorial="dialog"]').length === 1) {
+		icon = $('[data-tutorial="dialog"]').data('icon') || 'fa fa-graduation-cap';
+		container.innerHTML += '<a style="font-size:1.2em" href="#popupTutorial" title="Anleitung" data-rel="popup" data-position-to="window" data-transition="pop"><i class="' + icon + '" aria-hidden="true"></i></a>';
+	}
+
+	return container;
+}
 
 // -----------------------------------------------------------------------------
 
@@ -83,6 +77,12 @@ export function init(initialSettings) {
 		}
 	}
 
+	var ControlInfo = L.Control.extend({
+		options: {
+			position: 'bottomright',
+		},
+		onAdd: onAddCallback,
+	});
 	map.get().addControl(new ControlInfo());
 
 	$("#popupShare").on('popupafteropen', function () {
